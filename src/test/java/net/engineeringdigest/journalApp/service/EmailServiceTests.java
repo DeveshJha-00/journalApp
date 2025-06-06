60f6d2535a4fd745cdc74419b1a44a4dff79b8e7
DeveshJha-00 <deveshsanjiv16@gmail.com>
2025-06-06 17:14:08 +0530

    test mail

--- src/test/java/net/engineeringdigest/journalApp/service/EmailServiceTests.java
+++ src/test/java/net/engineeringdigest/journalApp/service/EmailServiceTests.java
@@ -1,6 +1,7 @@
 package net.engineeringdigest.journalApp.service;
 
 import net.engineeringdigest.journalApp.services.EmailService;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ public class EmailServiceTests {
     @Autowired
     private EmailService emailService;
 
+    @Disabled
     @Test
     void testSendMail(){
         emailService.sendEmail("cafog63099@lewou.com","Testing Email Service",
