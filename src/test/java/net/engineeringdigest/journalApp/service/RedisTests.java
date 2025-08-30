b63130e2d91529fd3215af3f5f82e3e6aa50243f
DeveshJha-00 <deveshsanjiv16@gmail.com>
2025-08-30 23:36:59 +0530

    redis test fix

--- src/test/java/net/engineeringdigest/journalApp/service/RedisTests.java
+++ src/test/java/net/engineeringdigest/journalApp/service/RedisTests.java
@@ -1,21 +1,49 @@
 package net.engineeringdigest.journalApp.service;
 
+import net.engineeringdigest.journalApp.services.RedisService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+
 public class RedisTests {
 
-    @Autowired
-    private RedisTemplate redisTemplate;
+    @Mock
+   private RedisTemplate<String, String> redisTemplate;
 
-    @Disabled
+   @Mock
+   private ValueOperations <String, String> valueOperations;
+
+    @InjectMocks
+    private RedisService myRedisService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Disabled("Disabled for CI/CD - mocked test, no real Redis call")
     @Test
-    void testRedis(){
-        String name = (String) redisTemplate.opsForValue().get("name");
-        System.out.println("Name from Redis: " + name);
+    void testRedisGet() {
+        // arrange
+        when(redisTemplate.opsForValue()).thenReturn(valueOperations);
+        when(valueOperations.get("name")).thenReturn("mockedAshu");
+
+        // act
+        String name = redisTemplate.opsForValue().get("name");
+
+        // assert
+        assertEquals("mockedAshu", name);
+        System.out.println("Name from mocked Redis: " + name);
     }
 }
