0299b89da55b8babbaf5fc6a63959f944010b085
DeveshJha-00 <deveshsanjiv16@gmail.com>
2025-08-30 10:41:54 +0530

    pls2

--- src/test/java/net/engineeringdigest/journalApp/service/EmailServiceTests.java
+++ src/test/java/net/engineeringdigest/journalApp/service/EmailServiceTests.java
@@ -8,6 +8,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.test.context.ActiveProfiles;
