57bc4d3d437472f9af880bf74af9283916c84616
DeveshJha-00 <deveshsanjiv16@gmail.com>
2025-08-30 11:18:03 +0530

    without disabled 2 ?

--- src/test/java/net/engineeringdigest/journalApp/JournalAppApplicationTests.java
+++ src/test/java/net/engineeringdigest/journalApp/JournalAppApplicationTests.java
@@ -1,12 +1,15 @@
 package net.engineeringdigest.journalApp;
 
+import net.engineeringdigest.journalApp.config.TestMailConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("dev")
+@Import(TestMailConfig.class)
 //@Disabled("Disabled for CI/CD - context application bean issues")
 class JournalAppApplicationTests {
 
--- src/test/java/net/engineeringdigest/journalApp/service/UserServiceTests.java
+++ src/test/java/net/engineeringdigest/journalApp/service/UserServiceTests.java
@@ -1,17 +1,20 @@
 package net.engineeringdigest.journalApp.service;
 
+import net.engineeringdigest.journalApp.config.TestMailConfig;
 import net.engineeringdigest.journalApp.entity.User;
 import net.engineeringdigest.journalApp.repository.UserRepository;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 //JUNIT TESTING
 @SpringBootTest
+@Import(TestMailConfig.class)
 //@Disabled("Disabled for CI/CD - requires database connection")
 public class UserServiceTests {
 
