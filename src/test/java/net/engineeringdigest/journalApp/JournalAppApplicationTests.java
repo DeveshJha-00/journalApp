a32e60923d8274ae87834360863303b65ae1c24f
DeveshJha-00 <deveshsanjiv16@gmail.com>
2025-08-30 10:31:22 +0530

    added profile

--- src/test/java/net/engineeringdigest/journalApp/JournalAppApplicationTests.java
+++ src/test/java/net/engineeringdigest/journalApp/JournalAppApplicationTests.java
@@ -3,8 +3,10 @@ package net.engineeringdigest.journalApp;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class JournalAppApplicationTests {
 
 	@Disabled("Disabled for CI/CD - requires database connection")
